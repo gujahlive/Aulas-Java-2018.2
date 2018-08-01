2ca62e715612fc39489a371e60b213a9fcb9e980
Gustavo Ferreira <gujahlive@users.noreply.github.com>
2018-08-01 11:27:41 -0300

    subindo fibbonnacci
    na duvida coloquei 2 letras de cada

--- PrimeiraAplicacao/src/primeiraaplicacao/Programa.java
+++ PrimeiraAplicacao/src/primeiraaplicacao/Programa.java
@@ -19,6 +19,45 @@ public class Programa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+                
+        int e = Integer.parseInt(JOptionPane.showInputDialog("1 - FATORIAL \n 2 - FIBBONACCI"));
+        switch(e){
+            case 1: 
+                 int numdigitado,fat;
+                 int n = Integer.parseInt(JOptionPane.showInputDialog("Digite um Valor"));
+        
+                numdigitado = n;	
+                for( fat = 1; n > 1; n = n - 1){
+
+                fat = fat * n;
+
+                }
+
+        System.out.println("numero digitado: "+ numdigitado +" fatorial: " + fat);
+                
+                
+            break;
+            
+            
+            case 2: 
+                   
+            int i,a=1,b=0,c=0,d=0,qtTermos;
+            qtTermos =Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de termos pro fibonacci: "));
+            
+
+            for(i=0;i<qtTermos;i++){
+                    c = a+b;
+                    System.out.println(c);
+                    a = b;
+                    b = c;
+
+
+            }
+
+                
+            break;            
+        
+        }
         int x = Integer.parseInt(JOptionPane.showInputDialog("Digite um Valor"));
        // int x = 10;
         int y = 20;
