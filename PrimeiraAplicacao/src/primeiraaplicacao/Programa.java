c0b81ce301869755918054e099469361b4f2a799
Gustavo Ferreira <gustavofvieira@live.com>
2018-08-06 09:20:32 -0300

    subindo

--- PrimeiraAplicacao/src/primeiraaplicacao/Programa.java
+++ PrimeiraAplicacao/src/primeiraaplicacao/Programa.java
@@ -7,6 +7,7 @@
  */
 package primeiraaplicacao;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -20,7 +21,7 @@ public class Programa {
      */
     public static void main(String[] args) {
                 
-        int e = Integer.parseInt(JOptionPane.showInputDialog("1 - FATORIAL \n 2 - FIBBONACCI"));
+        int e = Integer.parseInt(JOptionPane.showInputDialog("1 - FATORIAL \n 2 - FIBBONACCI\n 3 - Maior menor \n 4 - o pvt joao fez"));
         switch(e){
             case 1: 
                  int numdigitado,fat;
@@ -53,17 +54,60 @@ public class Programa {
 
 
             }
-
                 
-            break;            
-        
+                
+            break;
+
+            case 3: 
+                 int n11 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Valor"));
+                 int n22 = Integer.parseInt(JOptionPane.showInputDialog("Digite um Valor"));
+                 if(n11>n22){
+                       System.out.println(n11 + " Maior " +n22+" Menor");
+                 }else if(n22>n11){
+                     System.out.println(n22 + " maior " +n11+" Menor");
+                  
+                 }else if(n22==n11){
+                     System.out.println("Numeros Iguais");
+                 
+                 }
+                break;
+             case 4: 
+                  int n1,n2,cont = 0;
+        Scanner teclado = new Scanner(System.in);
+        do{
+            System.out.println("Informe o primeiro  valor:");
+            n1=teclado.nextInt();
+            System.out.println("Informe o segundo valor:");
+            n2=teclado.nextInt();
         }
-        int x = Integer.parseInt(JOptionPane.showInputDialog("Digite um Valor"));
+        while((n2<0 || n1<0)|| (n2==n1));
+        
+        if(n1<n2){
+            for(int i=n1;i<=n2;i++){
+                if((i%2!=0) && (i%3==0)){
+                  cont++;
+                }
+            }
+        }
+        
+        else if(n2<n1){
+            for(int i=n2;i<=n1;i++){
+                if((i%2!=0) && (i%3==0)){
+                  cont++;
+                }
+            }
+        }
+    System.out.printf("Existem %d números compatives\n",cont);
+               break;
+
+        
+        
+        /*int x = Integer.parseInt(JOptionPane.showInputDialog("Digite um Valor"));
        // int x = 10;
         int y = 20;
         int r = x + y;
         System.out.println("x: "+x+" y: "+y+" Resultado: " +r+" ou "+(x+y));
-        System.out.println("Ctrl + espaço = importa");
+        System.out.println("Ctrl + espaço = importa");*/
         
        
         // TODO code application logic here
