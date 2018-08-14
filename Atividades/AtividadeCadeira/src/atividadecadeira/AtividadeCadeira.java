c688d40b330717b434c6e79c1beeacb6eee65fb2
Gustavo Ferreira <gustavofvieira@live.com>
2018-08-14 08:26:52 -0300

    subindo

--- Atividades/AtividadeCadeira/src/atividadecadeira/AtividadeCadeira.java
+++ Atividades/AtividadeCadeira/src/atividadecadeira/AtividadeCadeira.java
@@ -18,14 +18,23 @@ public class AtividadeCadeira {
      */
     public static void main(String[] args) {
         Cadeira cadeira1 = new Cadeira();
-        cadeira1.cor = JOptionPane.showInputDialog("Qual a cor da cadeira?");
-        cadeira1.material = JOptionPane.showInputDialog("Qual o material da cadeira?");
-        cadeira1.altura = Float.parseFloat(JOptionPane.showInputDialog("Qual a Altura da cadeira?"));
-        int braco = Integer.parseInt(JOptionPane.showInputDialog("BRAÇO\n1 - Sim \n 0 - Não"));
-        cadeira1.atribuirValor();
+        String cor = JOptionPane.showInputDialog("Qual a cor da cadeira?");
+        //cadeira1.cor = JOptionPane.showInputDialog("Qual a cor da cadeira?");
+       // cadeira1.material = JOptionPane.showInputDialog("Qual o material da cadeira?");
+        String material = JOptionPane.showInputDialog("Qual o material da cadeira?");
+        //cadeira1.altura = Float.parseFloat(JOptionPane.showInputDialog("Qual a Altura da cadeira?"));
+        float altura = Float.parseFloat(JOptionPane.showInputDialog("Qual a Altura da cadeira?"));
+        boolean braco;
+        int verifica = JOptionPane.showConfirmDialog(null, "a dadeira tem braço?");
+        if(verifica == 1){
+            braco = false;
+        }else braco = true;
+        
+        cadeira1.personalizar(cor, material, braco, altura);
+        cadeira1.precificar();
             
         
-        System.out.println("Valor: "+cadeira1.valor);
+        System.out.println("Valor: "+cadeira1.valor+" Braço: "+cadeira1.braco);
         System.out.println("Cor: "+cadeira1.cor+ " Material: "+cadeira1.material +" Cadeira: " +cadeira1.braco+ " Altura: "+cadeira1.altura);
         // TODO code application logic here
     }
--- Atividades/AtividadeCadeira/src/atividadecadeira/Cadeira.java
+++ Atividades/AtividadeCadeira/src/atividadecadeira/Cadeira.java
@@ -20,20 +20,24 @@ public class Cadeira {
     
     
     void personalizar(String cor, String material, boolean braco, float altura){
-        if(this.braco == true){
-             this.valor *= 0.2; 
-           
-        }        
+        this.cor = cor;
+        this.altura = altura;
+        this.braco = braco;
+        this.material = material;
     
     }
     
-    void atribuirValor(){
-        if(this.altura > 0.1 && this.altura <= 1 ){
+    void precificar(){
+        if(this.altura > 0 && this.altura <= 1 ){
             this.valor = 50; 
         }else if(this.altura > 1 && this.altura <= 1.5 ){
             this.valor = 100; 
         }else if(this.altura > 1.5 ){
             this.valor = 150; 
+        } 
+         if(this.braco){
+             this.valor += this.valor*0.2; 
+           
         }
         
     }
