5388d7910e7a8a87d55df9b8af019cf7aa86a449
Gustavo Ferreira <gustavofvieira@live.com>
2018-08-20 10:12:14 -0300

    Atividade Celular

--- Atividades/AtividadeCelular/src/atividadecelular/Celular.java
+++ Atividades/AtividadeCelular/src/atividadecelular/Celular.java
@@ -10,5 +10,47 @@ package atividadecelular;
  * @author aluno
  */
 public class Celular {
+    private float preco;
+    private String modelo, marca;
+    private int GBarm;
+    public Celular(String modelo, String marca, int GBarm){
+        this.modelo = modelo;
+        this.marca = marca;
+        this.GBarm = GBarm;
+    }
+    public Celular (float preco,String modelo, String marca, int GBarm){
+        this.preco = preco;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.GBarm = GBarm;
+     
+    }
+    
+    public void setPreco(float preco){
+        this.preco = preco;
+    }
+    public float getPreco(){
+        return this.preco;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public String getMarca(){
+        return this.marca;
+    }
+    
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setGBarm(int GBarm){
+        this.GBarm = GBarm;
+    }
+    public int getGBarm(){
+        return this.GBarm;
+    }
+
     
 }
--- Atividades/AtividadeCelular/src/atividadecelular/Programa.java
+++ Atividades/AtividadeCelular/src/atividadecelular/Programa.java
@@ -5,6 +5,8 @@
  */
 package atividadecelular;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author aluno
@@ -15,6 +17,19 @@ public class Programa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      // 
+        
+      int e = Integer.parseInt(JOptionPane.showInputDialog("1 -FABRICA\n2- LOJA"));
+      switch(e){
+          case 1:
+               Celular celular = new Celular(JOptionPane.showInputDialog("modelo:"),JOptionPane.showInputDialog("Marca: "),Integer.parseInt(JOptionPane.showInputDialog("Quantidade de Armazenamento em GigaByte: ")));
+               System.out.println("\nMarca: "+celular.getMarca()+"\nModelo: "+celular.getModelo()+"\nQuantidade de Armazenameto: "+celular.getGBarm()+" GB");
+               break;
+          case 2:
+              Celular celularPreco = new Celular(Float.parseFloat(JOptionPane.showInputDialog("Preço: ")),JOptionPane.showInputDialog("modelo:"),JOptionPane.showInputDialog("Marca: "),Integer.parseInt(JOptionPane.showInputDialog("Quantidade de Armazenamento em GigaByte: ")));
+              System.out.println("Preço: "+celularPreco.getPreco()+"\nMarca: "+celularPreco.getMarca()+"\nModelo: "+celularPreco.getModelo()+"\nQuantidade de Armazenameto: "+celularPreco.getGBarm()+" GB");
+              break;
+      }
         // TODO code application logic here
     }
     
