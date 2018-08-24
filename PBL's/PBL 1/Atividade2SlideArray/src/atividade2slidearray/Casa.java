b53f052ba07094142ebdfee061e546b6563652ef
gustavo ferreira <gustavofvieira@live.com>
2018-08-24 12:30:54 -0300

    subindo atv 2

---
+++ PBL's/PBL 1/Atividade2SlideArray/src/atividade2slidearray/Casa.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade2slidearray;
+
+/**
+ *
+ * @author ACQUA
+ */
+public class Casa {
+    private String cor;
+    private int qtdPortas;
+    
+    private int qtdPortasAbertas;
+    private int totalDePortas;
+    
+    public void pinta(String s){
+        
+    }
+    public int quantasPortasEstaoAbertas(){
+        return this.qtdPortasAbertas;
+    }
+    public int totalDePortas(){
+        return this.totalDePortas;
+    }
+    public void cadastraPortas(Porta porta){
+    
+    }
+    
+}
---
+++ PBL's/PBL 1/Atividade2SlideArray/src/atividade2slidearray/Porta.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade2slidearray;
+    
+/**
+ *
+ * @author ACQUA
+ */
+public class Porta {
+    
+    private boolean aberta;
+    private String cor;
+    private float dimensaoX;
+    private float dimensaoY;
+    private float dimensaoZ;
+    
+    public void fecha(){
+    }
+    public void pinta(String s){
+    }
+    public boolean estaAberta(){
+        return this.aberta;
+    }
+    
+    
+    
+    
+}
