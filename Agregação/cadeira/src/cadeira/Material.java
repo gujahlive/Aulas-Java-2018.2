f45f0bfd3a9e74d5a8b83a5ba0be857a41a31484
Gustavo Ferreira <gustavofvieira@live.com>
2018-08-27 10:10:51 -0300

    Subindo associação

--- Agregação/cadeira/src/cadeira/Material.java
+++ Agregação/cadeira/src/cadeira/Material.java
@@ -10,5 +10,33 @@ package cadeira;
  * @author aluno
  */
 public class Material {
+    private double peso;
+    private double preco;
+    private String cor;
+    
+   public void setCor(String cor){
+        this.cor = cor;
+    }
+    
+    public String getCor (){
+        return this.cor;
+    }
+    
+   public void setPreco(double preco){
+        this.preco = preco ;
+    }
+    
+    public double getPreco (){
+        return this.preco;
+    } 
+    
+    
+    public void setPeso(double peso){
+        this.peso = peso ;
+    }
+    
+    public double getPeso (){
+        return this.peso;
+    }
     
 }
--- Agregação/cadeira/src/cadeira/Programa.java
+++ Agregação/cadeira/src/cadeira/Programa.java
@@ -5,6 +5,8 @@
  */
 package cadeira;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author aluno
@@ -15,6 +17,39 @@ public class Programa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        int i;
+        Cadeira cadeira = new Cadeira();
+    
+        
+        boolean braco;
+          
+          //  cadeira.setCor(JOptionPane.showInputDialog("Cor: "));
+         //   cadeira.setMaterial(JOptionPane.showInputDialog("Material: "));
+            cadeira.setAltura(Float.parseFloat(JOptionPane.showInputDialog("Altura: ")));
+            int verifica = JOptionPane.showConfirmDialog(null, "a dadeira tem braço?");
+            if(verifica == 1){
+                  braco = false;
+             }else braco = true;
+            cadeira.setBraco(braco);
+            cadeira.precificar();
+ 
+            
+             Material material = new Material();
+             material.setCor("Azivis");
+             material.setPeso(0.8);
+             material.setPreco(40);
+             cadeira.setMaterial(material);
+    
+        
+    
+    
+            System.out.println("Material: "+cadeira.getMaterial()+" Cor: "+cadeira.getMaterial().getCor()+ " Altura: "+cadeira.getAltura()+" Braço: "+cadeira.getBraco()+" Valor: "+cadeira.getValor());
+            System.out.println(" Cor: "+cadeira.getMaterial().getCor()+"Peso: "+cadeira.getMaterial().getPeso()+"Preço Material: "+cadeira.getMaterial().getPreco());
+       
+            
+        
+        
         // TODO code application logic here
     }
     
