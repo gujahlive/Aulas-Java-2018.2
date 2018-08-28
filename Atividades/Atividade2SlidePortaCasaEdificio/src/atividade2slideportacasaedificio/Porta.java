c29008618690455ce95ad9f61a51b25659b556fd
Gustavo Ferreira <gustavofvieira@live.com>
2018-08-28 09:10:47 -0300

    subindo atividade 2 lista

---
+++ Atividades/Atividade2SlidePortaCasaEdificio/src/atividade2slideportacasaedificio/Porta.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade2slideportacasaedificio;
+
+/**
+ *
+ * @author aluno
+ */
+public class Porta {
+   private boolean aberta;
+   private String cor;
+   private float dimensaoX;
+   private float dimensaoY;
+   private float dimensaoz;
+   
+   public void abre(){
+   
+   }
+    public void fecha(){
+   
+   }
+   public void pinta(String s){
+   
+   }
+   public boolean estaAberta(){
+      return true;
+   }
+   
+   
+   
+    
+}
