8996b495b83c1c2067be49f399eb3ae9b472008b
Gustavo ferreira <gustavofvieira@live.com>
2018-09-04 08:57:19 -0300

    subindo classe abstrata

---
+++ Classes Abstratas/PetShop/src/petshop/Dono.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petshop;
+
+/**
+ *
+ * @author aluno
+ */
+public class Dono {
+    private String nome;
+    private String telefone;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
---
+++ Classes Abstratas/PetShop/src/petshop/Pet.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petshop;
+
+/**
+ *
+ * @author aluno
+ */
+public abstract class Pet {
+    
+    protected String nome;
+    protected float peso;
+    protected String sexo;
+    protected int idade;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+    
+}
